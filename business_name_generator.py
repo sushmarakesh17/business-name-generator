85c7372717a0c9aab027b61f2eec670f8a8be5ff
sushmarakesh <sushma.w.k17@gmail.com>
2026-07-09 12:28:29 +0530

    Add files via upload

---
+++ business_name_generator.py
@@ -0,0 +1,8 @@
+import random
+adjectives = ["Smart", "Bright", "Golden", "Creative", "Royal"]
+nouns = ["Tech", "Solutions", "Bakery", "Foods", "Creations"]
+word1 = random.choice(adjectives)
+word2 = random.choice(nouns)
+business_name = word1 + " " + word2
+print("Suggested Business Name:", business_name)    
+#go to terminal->  new terminal-> type python business_name_generator.py->press enter
